ee874148260ceab05dd3a18a88ef46bea597228d
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-11-14 14:47:05 -0300

    third commit

--- src/http/controllers/user.ts
+++ src/http/controllers/user.ts
@@ -1,22 +1,29 @@
 import { authConfig } from "@/config/auth";
 import { env } from "@/env";
+import { User } from "@prisma/client";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export class UserController {
   async authenticate(req: Request, res: Response) {
     try {
+      if (!req.user) {
+        return res.status(401).json({ error: "User not authenticated" });
+      }
+
+      const { id } = req.user as User;
+
       const accessToken = jwt.sign({}, authConfig.secret, {
-        subject: req.user.id,
-        expiresIn: authConfig.expiresIn["7d"],
+        subject: id,
+        expiresIn: authConfig.expiresIn["1h"],
       });
 
       const refreshToken = jwt.sign({}, authConfig.secret, {
-        subject: req.user.id,
+        subject: id,
         expiresIn: authConfig.expiresIn["7d"],
       });
 
-      const MILLISECONDS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
+      const MILLISECONDS_IN_ONE_HOUR = 1000 * 60 * 60;
       const MILLISECONDS_IN_SEVEN_DAYS = 1000 * 60 * 60 * 24 * 7;
 
       res.cookie("accessToken", accessToken, {
@@ -24,7 +31,7 @@ export class UserController {
         secure: true,
         sameSite: "strict",
         httpOnly: true,
-        maxAge: MILLISECONDS_IN_ONE_DAY,
+        maxAge: MILLISECONDS_IN_ONE_HOUR,
       });
 
       res.cookie("refreshToken", refreshToken, {
@@ -35,8 +42,25 @@ export class UserController {
         maxAge: MILLISECONDS_IN_SEVEN_DAYS,
       });
 
-      res.redirect(env.BASE_CLIENT_URL + "/");
-      const { user } = req;
+      return res.redirect(env.BASE_CLIENT_URL + "/");
     } catch (error) {}
+  }
+
+  async logout(req: Request, res: Response) {
+    res.clearCookie("accessToken", {
+      path: "/",
+      secure: true,
+      sameSite: "strict",
+      httpOnly: true,
+    });
+
+    res.clearCookie("refreshToken", {
+      path: "/",
+      secure: true,
+      sameSite: "strict",
+      httpOnly: true,
+    });
+
+    return res.status(200).json();
   }
 }
--- src/http/routes/discord.router.ts
+++ src/http/routes/discord.router.ts
@@ -1,55 +1,19 @@
 import { Router } from "express";
 import passport from "passport";
-import jwt from "jsonwebtoken";
-import { authConfig } from "@/config/auth";
-import { env } from "@/env";
+import { UserController } from "../controllers/user";
 
 export const discordRouter = Router();
+const userController = new UserController();
 
 discordRouter.get(
   "/discord",
   passport.authenticate("discord", { scope: ["identify", "email"] })
 );
 
-discordRouter.get("/logout", (req, res) => {
-  res.send("Logout");
-});
+discordRouter.get("/logout", userController.logout);
 
 discordRouter.get(
   "/discord/redirect",
   passport.authenticate("discord", { session: false }),
-  (req, res) => {
-    console.log("req", req.user);
-
-    const accessToken = jwt.sign({}, authConfig.secret, {
-      subject: req.user.id,
-      expiresIn: authConfig.expiresIn["7d"],
-    });
-
-    const refreshToken = jwt.sign({}, authConfig.secret, {
-      subject: req.user.id,
-      expiresIn: authConfig.expiresIn["7d"],
-    });
-
-    const MILLISECONDS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
-    const MILLISECONDS_IN_SEVEN_DAYS = 1000 * 60 * 60 * 24 * 7;
-
-    res.cookie("accessToken", accessToken, {
-      path: "/",
-      secure: true,
-      sameSite: "strict",
-      httpOnly: true,
-      maxAge: MILLISECONDS_IN_ONE_DAY,
-    });
-
-    res.cookie("refreshToken", refreshToken, {
-      path: "/",
-      secure: true,
-      sameSite: "strict",
-      httpOnly: true,
-      maxAge: MILLISECONDS_IN_SEVEN_DAYS,
-    });
-
-    res.redirect(env.BASE_CLIENT_URL + "/");
-  }
+  userController.authenticate
 );
